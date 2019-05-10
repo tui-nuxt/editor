e569615b6f44cc8f64a674feb1a939e2ffecfadc
김용건 <mail@kyg.kr>
2019-05-10 10:17:40 +0900

    chore: change mode spa to universal

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path')
 
 module.exports = {
-  mode: 'spa',
+  mode: 'universal',
   rootDir: resolve(__dirname, '../../'),
   srcDir: resolve(__dirname),
   buildDir: resolve(__dirname, '../../.nuxt'),
