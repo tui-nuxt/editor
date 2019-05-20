8218f5dd7a6f8ef0b60e38d96d154d1d9b995148
김용건 <mail@kyg.kr>
2019-05-20 14:58:35 +0900

    chore: nuxt-tui to tui-nuxt

--- sample/basic/pages/index.vue
+++ sample/basic/pages/index.vue
@@ -28,8 +28,8 @@ export default {
         '# 🍞📝 TOAST UI Editor for Nuxt.JS',
         '[**TOAST UI Editor for Vue**](https://github.com/nhn/toast-ui.vue-editor) wrapping module',
         ' \
-          ![version](https://img.shields.io/npm/v/@nuxt-tui/editor.svg?style=flat-square) \
-          ![License](https://img.shields.io/github/license/nuxt-tui/editor.svg?style=flat-square) \
+          ![version](https://img.shields.io/npm/v/@tui-nuxt/editor.svg?style=flat-square) \
+          ![License](https://img.shields.io/github/license/tui-nuxt/editor.svg?style=flat-square) \
           ![Code style](https://img.shields.io/badge/code%20style-airbnb-success.svg?style=flat-square) \
         ',
       ].join('\n')
